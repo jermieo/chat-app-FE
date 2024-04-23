7a9d509ac0af566f01839fb232e48e3d6cba3c77
jermieo <jermieorex1798@gmail.com>
2024-04-24 02:10:03 +0530

    remove console

--- src/Components/SetAvatar.jsx
+++ src/Components/SetAvatar.jsx
@@ -33,7 +33,6 @@ export default function SetAvatar() {
       const { data } = await axios.post(`${setAvatarRoute}/${user._id}`, {
         image: avatars[selectedAvatar],
       });
-      console.log(data, "dataaa");
       if (data.isSet) {
         user.isAvatarImageSet = true;
         user.avatarImage = data.image;
